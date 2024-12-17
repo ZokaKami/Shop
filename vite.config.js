7d1648fa532da4166649ee79fa0a068b75e40220
ZokaKami <ruffy92@live.com>
2024-12-17 10:53:34 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': '/src', // Example alias for cleaner imports
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
+});
